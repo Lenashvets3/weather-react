432d6bc0768b3410bf4abbe2e744ba61bc72a4e0
Lenashvets3 <Lenashvets3@gmail.com>
2023-03-27 11:23:00 +0200

    Added WeatherInfo.js, WeatherTemperature.js files

---
+++ src/WeatherInfo.js
@@ -0,0 +1,43 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1 id="city-name">
+        {props.data.city}
+        <br />
+      </h1>
+
+      <h1 id="time">
+        <ul>
+          <li>
+            <FormattedDate date={props.data.date} />
+          </li>
+        </ul>
+      </h1>
+
+      <div className="currentTemp">
+        <WeatherTemperature celsius={props.data.temperature} />
+      </div>
+      <img
+        alt={props.data.description}
+        src={`http://openweathermap.org/img/wn/${props.data.iconUrl}@2x.png`}
+        id="icon"
+      />
+
+      <ul className="indicators">
+        <li>
+          • <span id="description">{props.data.description}</span>
+        </li>
+        <li>
+          • humidity: <span id="humidity">{props.data.humidity}</span>%
+        </li>
+        <li>
+          • wind: <span id="wind">{props.data.wind}</span>km/hr
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/WeatherTemperature.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <span className="WeatherTemperature">
+        <span>{Math.round(props.celsius)}</span>
+        <span>
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </span>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <span className="WeatherTemperature">
+        <span>{Math.round(fahrenheit)}</span>
+        <span>
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>
+          {""}| °F
+        </span>
+      </span>
+    );
+  }
+}
