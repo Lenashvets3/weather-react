830ddbe351bf3df32f55e98f2b57913493b6dc42
Lenashvets3 <Lenashvets3@gmail.com>
2023-03-27 11:31:30 +0200

    Created Weather.js and css files, FarmattedDate.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,40 +1,81 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./Weather.css";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
-export default function Weather() {
-  const [temperature, setTemperature] = useState("");
-  const [description, setDescription] = useState("");
-  const [humidity, setHumidity] = useState("");
-  const [wind, setWind] = useState("");
-  const [city, setCity] = useState("");
-
-  function showTemperature(response) {
-    setTemperature(Math.round(response.data.main.temp));
-    setDescription(response.data.weather[0].description);
-    setHumidity(response.data.main.humidity);
-    setWind(Math.round(response.data.wind.speed));
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      iconUrl: response.data.weather[0].icon,
+      city: response.data.name,
+    });
   }
+
+  function search() {
+    let apiKey = "9e55ce91de1e84df9f3dbe33f9c2133a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "loading...";
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5b74d10f3ef03caf1ac640b557c288c3&units=metric`;
-    axios.get(url).then(showTemperature);
+    search();
   }
 
-  function changeCity(event) {
+  function handleCityChange(event) {
     setCity(event.target.value);
   }
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="search" onChange={changeCity} />
-        <input type="submit" value="Search" />
-      </form>
-      <ul>
-        <li>Temperature: {temperature}°C</li>
-        <li>Description: {description}</li>
-        <li>Humidity: {humidity}%</li>
-        <li>Wind: {wind} km/h</li>
-      </ul>
-    </div>
-  );
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="cityBox">
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Change City..."
+                onChange={handleCityChange}
+              />
+
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </form>
+          <WeatherInfo data={weatherData} />
+          <WeatherForecast coordinates={weatherData.coordinates} />
+          <footer>
+            <a
+              href="https://github.com/Lenashvets3/weather-react"
+              alt="weather-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open-source Code
+            </a>
+            <span className="name"> by Olena Shvets</span>
+          </footer>
+        </div>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
